c35191db71decdfec4e4c3963ed8d1df5a5b31a7
Sean Fitzgerald <fitzgerald.sean.t@gmail.com>
2018-12-23 20:59:34 -0500

    Initial Commit

---
+++ AI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AI
+//
+//  Created by Sean Fitzgerald on 12/23/18.
+//  Copyright © 2018 Sean Fitzgerald. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
